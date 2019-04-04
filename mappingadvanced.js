af57c3c84a81510c639b3fa3745fe2d90b260361
Lzduque <lzduque@hotmail.com>
2019-04-04 17:44:42 +0000

    adjusting to deliver in the correct format mapping advanced

--- mappingadvanced.js
+++ mappingadvanced.js
@@ -1,16 +1,39 @@
 var words = ["ground", "control", "to", "major", "tom"];
 
-var lengths = words.map(word => word.length);
-console.log(lengths);
+var newMap = function(data, fnName) {
+  var newArray = [];
+  data.forEach((value) => newArray.push(fnName(value)));
+  return newArray;
+}
 
+var countLength = newMap(words, function(word) {
+  return word.length;
+});
 
-var upperCase = words.map(function(word) {
+var upperCase = newMap(words, function(word) {
   return word.toUpperCase();
 });
-console.log(upperCase);
 
-var reverse = words.map(word => word.split('').reverse().join(''));
-console.log(reverse);
+var reverseWords = newMap(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
+console.log(countLength);
+console.log(upperCase);
+console.log(reverseWords);
+
+
+// var lengths = words.map(word => word.length);
+// console.log(lengths);
+
+
+// var upperCase = words.map(function(word) {
+//   return word.toUpperCase();
+// });
+// console.log(upperCase);
+
+// var reverse = words.map(word => word.split('').reverse().join(''));
+// console.log(reverse);
 
 // //var wordsLength  = function(word) {
 //   return word.length;
